e5d0c362e9882493425ba2803a359d1855fb80df
Yunus Emre Çoban <91698352+MorsCrt@users.noreply.github.com>
2022-04-13 22:17:54 -0700

    Delete pi2.sql

--- pi2.sql
+++
@@ -1,9 +0,0 @@
-DO $$
-DECLARE  
-   a integer :=  position('//' in '<url>https://www.w3schools.com</url>')+2;  
-   d integer :=  position('com' in '<url>https://www.w3schools.com</url>')+3;  
-   c varchar(50);  
-BEGIN  
-   c :=  SUBSTRING( '<url>https://www.w3schools.com</url>',a , d-a);
-    RAISE NOTICE'Value of c: %', c;	
-END $$;
