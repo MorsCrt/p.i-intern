cc0c54d0a0186a120181cc90f77c9507a87cf513
MorsCrt <203301130@ogr.selcuk.edu.tr>
2022-04-13 17:13:00 -0700

    impute-zero

---
+++ impute.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv("country_vaccination_stats.csv",low_memory=False)
+
+# Check missing data:
+# print(impute_df.isnull().sum())
+
+def impute_zero(dataframe,column_name:str) -> pd.DataFrame:
+    dataframe[column_name] = dataframe[column_name].fillna(0)
+    return dataframe
+
+impute_df = impute_zero(df,"daily_vaccinations")
+print(impute_df.isnull().sum())
